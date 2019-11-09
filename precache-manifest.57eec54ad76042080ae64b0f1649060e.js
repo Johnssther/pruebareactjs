4117740ef7e46887fc0dc64dfb454ca52b189e0c
Johnssther <j.ahr@hotmail.com>
2019-11-09 06:53:53 -0500

    Updates

---
+++ precache-manifest.57eec54ad76042080ae64b0f1649060e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1a15d21a958b7330c4c75287169dfd",
+    "url": "/pruebareactjs/index.html"
+  },
+  {
+    "revision": "3b1e2bd42884ce2ef31f",
+    "url": "/pruebareactjs/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "947abe3e4a62357c714a",
+    "url": "/pruebareactjs/static/js/2.017bb613.chunk.js"
+  },
+  {
+    "revision": "3b1e2bd42884ce2ef31f",
+    "url": "/pruebareactjs/static/js/main.3732f763.chunk.js"
+  },
+  {
+    "revision": "0ccb5b99bbb9f701d065",
+    "url": "/pruebareactjs/static/js/runtime-main.91d535bd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pruebareactjs/static/media/logo.25bf045c.svg"
+  }
+]);
